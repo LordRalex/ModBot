d85661a00242e33aefd9aba471347a5962f2957d
Joshua <Joshua@localhost.localdomain>
2012-12-02 22:56:05 -0600

    Add support to only parse emails from certain senders

--- src/main/java/us/lordralex/modbot/mail/Mail.java
+++ src/main/java/us/lordralex/modbot/mail/Mail.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.mail.Address;
 import javax.mail.Flags;
 import javax.mail.Folder;
 import javax.mail.Message;
@@ -13,6 +14,7 @@ import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
 import javax.mail.Session;
 import javax.mail.Store;
+import javax.mail.internet.InternetAddress;
 import javax.mail.search.FlagTerm;
 import org.pircbotx.PircBotX;
 import us.lordralex.modbot.Main;
@@ -25,14 +27,17 @@ import us.lordralex.modbot.scanner.FileExamine;
  */
 public class Mail extends Thread {
 
-    List<String> threads = new ArrayList<>();
-    Session session;
-    Store store;
+    private List<String> threads = new ArrayList<>();
+    private Session session;
+    private Store store;
+    private List<String> acceptedSenders = new ArrayList<>();
 
     public Mail() throws NoSuchProviderException {
         this.setName("Mail_Thread");
         session = Session.getDefaultInstance(System.getProperties(), null);
         store = session.getStore("imap");
+        acceptedSenders.add("noreply@curse.com");
+        acceptedSenders.add("scan@virustotal.com");
     }
 
     @Override
@@ -51,24 +56,28 @@ public class Mail extends Thread {
                     inbox.open(Folder.READ_WRITE);
                     Message[] messages = inbox.search(new FlagTerm(new Flags(Flags.Flag.SEEN), false));
                     for (Message message : messages) {
-                        try (BufferedReader reader = new BufferedReader(new InputStreamReader(message.getInputStream()))) {
-                            List<String> temp = new ArrayList<>();
-                            String line;
-                            while ((line = reader.readLine()) != null) {
-                                temp.add(line);
+                        Address[] froms = message.getFrom();
+                        String email = froms == null ? null : ((InternetAddress) froms[0]).getAddress();
+                        if (acceptedSenders.contains(email)) {
+                            try (BufferedReader reader = new BufferedReader(new InputStreamReader(message.getInputStream()))) {
+                                List<String> temp = new ArrayList<>();
+                                String line;
+                                while ((line = reader.readLine()) != null) {
+                                    temp.add(line);
+                                }
+                                threads.add(temp.get(3)
+                                        .replace("has just posted a new topic entitled", "posted")
+                                        .replace("\" in forum \"", "\" in \"")
+                                        .replace("has just posted a reply to a topic that you have subscribed to titled \"", "posted a reply to ")
+                                        .replace("The topic can be found here: ", "")
+                                        + " "
+                                        + temp.get(temp.size() - 8).split("-")[0]
+                                        + "-");
+                                FileExamine examine = new FileExamine();
+                                examine.start(temp.get(temp.size() - 8).split("-")[0] + "-");
+                            } catch (Exception e) {
+                                e.printStackTrace(System.out);
                             }
-                            threads.add(temp.get(3)
-                                    .replace("has just posted a new topic entitled", "posted")
-                                    .replace("\" in forum \"", "\" in \"")
-                                    .replace("has just posted a reply to a topic that you have subscribed to titled \"", "posted a reply to ")
-                                    .replace("The topic can be found here: ", "")
-                                    + " "
-                                    + temp.get(temp.size() - 8).split("-")[0]
-                                    + "-");
-                            FileExamine examine = new FileExamine();
-                            examine.start(temp.get(temp.size() - 8).split("-")[0] + "-");
-                        } catch (Exception e) {
-                            e.printStackTrace(System.out);
                         }
                         message.setFlag(Flags.Flag.SEEN, true);
                     }
