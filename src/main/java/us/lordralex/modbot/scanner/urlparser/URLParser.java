fc15cb0063cfb03171d6c239a7d66ee07a7b2104
Joshua <Joshua@localhost.localdomain>
2012-12-03 22:11:56 -0600

    Remove template comment

--- src/main/java/us/lordralex/modbot/scanner/urlparser/URLParser.java
+++ src/main/java/us/lordralex/modbot/scanner/urlparser/URLParser.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package us.lordralex.modbot.scanner.urlparser;
 
 import java.util.List;
--- src/main/java/us/lordralex/modbot/scanner/urlparser/parsers/Mediafire.java
+++ src/main/java/us/lordralex/modbot/scanner/urlparser/parsers/Mediafire.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package us.lordralex.modbot.scanner.urlparser.parsers;
 
 import java.util.ArrayList;
